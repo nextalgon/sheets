40b7cac1f46afc76e0a3d15077431fab1fcc1d48
Utkurbek Sodiqov <80362668+nextalgon@users.noreply.github.com>
2021-12-09 17:48:44 +0500

    Update bot_token.py

--- bot_token.py
+++ bot_token.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-token = '1990773412:AAGuJ2sCb16TCuAkg4K2k8s3sod4uYC16Xw'
+token = 'your token'
 
 bot = Bot(token=token)
 dp = Dispatcher(bot, storage=MemoryStorage())
